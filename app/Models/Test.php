ac696a3d6246b6f6d5fc934333c32326866483cc
Calvin-Proj <s218102909@mandela.ac.za>
2021-02-04 08:44:49 +0200

    Update Test.php

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -31,6 +31,7 @@ class Test extends Model
         ->join('module_user', 'modules.id', '=', 'module_user.module_id')
         ->join('venues', 'tests.venue_id', '=', 'venues.id')
         ->select('tests.*', 'venues.venue_name','modules.module_name')
+        ->where('module.module_id','modules.id')
         ->orderBy('created_at','desc')
         ->get();
         return $tests;
